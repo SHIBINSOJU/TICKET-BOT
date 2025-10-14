268ff04598b996e9eeca1a9d5331170621e4bd26
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2025-10-14 19:35:00 +0530

    Delete events/ready.js

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "ready",
-  once: true,
-  execute(client) {
-    console.log(`✅ Bot logged in as ${client.user.tag}`);
-    console.log(`🔹 Loaded ${client.commands.size} commands`);
-  },
-};
